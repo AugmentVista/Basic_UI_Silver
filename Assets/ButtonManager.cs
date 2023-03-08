1e1a07f89e856e686addbde76454781fc0f3e770
AugmentVista <AuAgments@gmail.com>
2023-03-08 12:36:58 -0400

    complete

--- Assets/ButtonManager.cs
+++ Assets/ButtonManager.cs
@@ -8,50 +8,22 @@ using TMPro;
 
 public class ButtonManager : MonoBehaviour
 {
-    public TextMeshProUGUI myTextMeshProButton;
-    private Color orginalColor;
+    private int score;
+    public TMPro.TMP_Text scoreText;
 
-
-    public static void Main(string[] args)
+    private void Start()
     {
-        // Start is called before the first frame update
-        void Start()
-        {
-            orginalColor = MyTextMeshProButton.color;
-            Button button = GetComponent<button>();
-            button.onClick.AddListener(OnButtonClick);
-
-            void OnButtonClick()
-            {
-                orginalColor currentColor = myTextMeshProButton.color;
-                currentColor.a = 1f;
-                currentColor.r += 0.2f;
-                currentColor.g += 0.2f;
-                currentColor.b += 0.2f;
-
-                myTextMeshPRoButton.color = currentColor;
-
-                StartCoroutine(ResetButtonColor());
-            }
-
-            IEnumerator ResetButtonColor()
-            {
-                yield return new WaitForSeconds(0.5f);
-                myTextMeshProButton.color = orginalColor;
-            }
-
-
-        }
+        score = 0;
+        scoreText.text = score.ToString();
     }
 
-    // Update is called once per frame
-    void Update()
+    public void ChangeScore(int change)
     {
-
+        score = score + change;
+        if (score < 0)
+        {
+            score = 0;
+        }
+        scoreText.text = score.ToString();
     }
 }
-
-
-
-
-
